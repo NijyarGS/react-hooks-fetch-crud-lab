c85bc28cb44149f87379eb2d0f82c3d397012bfa
Nijyar Ghazi <nijyar360@gmail.com>
2022-10-21 12:04:18 +0300

    Completed assigment

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,12 @@ import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [myData,setMyData] = useState([])
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm myData={myData} setMyData={setMyData}/> : <QuestionList myData={myData} setMyData={setMyData}/>}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React,{useEffect} from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+
+function QuestionList({myData,setMyData}) {
+  
+  const API_URL = 'http://localhost:4000/questions'
+  useEffect(()=>{
+
+
+      let fetchData = async() => {
+        const respond = await fetch(API_URL)
+        const data = await respond.json() 
+        setMyData(data)
+      }
+
+      fetchData()
+      
+      return(
+        clearInterval(fetchData)
+      )
+
+  },[API_URL, setMyData])
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+      { 
+       myData ? myData.map((question)=><QuestionItem key={question.id} question={question} myData={myData} setMyData={setMyData} />) : null 
+      }
+      </ul>
     </section>
   );
 }
